087d3bcc9b8a2877efc935e4b1f9e8840ad71918
root <root@ip-172-31-27-42.eu-west-1.compute.internal>
2019-04-03 21:36:24 +0000

    f

--- infra-engineer-test-master/test.sh
+++ infra-engineer-test-master/test.sh
@@ -3,5 +3,4 @@ set -e
 
 export DOCKER_HOST=127.0.0.1:2375
 docker-compose -f ./infra-engineer-test-master/docker-compose.yml build
-docker-compose run -f ./infra-engineer-test-master/docker-compose.yml --rm --no-deps api go test -v
-
+docker-compose -f ./infra-engineer-test-master/docker-compose.yml run --rm --no-deps api go test -v
