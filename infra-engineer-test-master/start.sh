4252aaf9f02d0d55c62a955eedc2fdadc61f9de2
root <root@ip-172-31-27-42.eu-west-1.compute.internal>
2019-04-03 20:13:01 +0000

    fox

--- infra-engineer-test-master/start.sh
+++ infra-engineer-test-master/start.sh
@@ -2,5 +2,8 @@
 set -e
 
 #docker-compose up --build -f ./docker-compose.yml --abort-on-container-exit
-docker-compose up 
+#docker-compose up  #--force-recreate --verbose
 #docker-compose -f services/docker-compose-jenkins.yml up \
+
+
+docker-compose -f ./infra-engineer-test-master/docker-compose.yml up --build -d
